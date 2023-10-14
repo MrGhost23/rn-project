56b48b0ea5785395a5481b823046f81f6d3774da
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-10-14 05:20:37 +0300

    create a modal for goal input

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { Button, FlatList, StyleSheet, View } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  const openModal = () => {
+    setModalIsVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalIsVisible(false);
+  };
 
   const addGoalHandler = (enteredGoal) => {
     setGoals((currentGoals) => [
       ...currentGoals,
       { text: enteredGoal, id: Math.random().toString() },
     ]);
+    closeModal();
   };
 
   const removeGoal = (id) => {
@@ -20,7 +30,12 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput addGoalHandler={addGoalHandler} />
+      <Button title="Add New Goal" color="#06B6D4" onPress={openModal} />
+      <GoalInput
+        addGoalHandler={addGoalHandler}
+        visible={modalIsVisible}
+        closeModal={closeModal}
+      />
       <View style={styles.goalsContainer}>
         <FlatList
           data={goals}
